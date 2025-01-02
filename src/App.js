ffdb61b50b173660e57b0f3ee016522e7c174909
kmatos7793 <88529154+kmatos7793@users.noreply.github.com>
2025-01-02 10:56:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "627265";
+  let InstanceId_Widget = "620079";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "41903";
+  let ProductId = "42053";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
